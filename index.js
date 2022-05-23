e1400e995ac3653e9df64011ec84e9ca99dce4e8
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-24 03:00:53 +0600

    make an api to get all products

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 require('dotenv').config()
 
@@ -9,6 +10,34 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.8dwzm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const productsCollection = client.db('manufacturerWebsite').collection('products');
+
+
+
+        // ALL GET Method
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const productsArray = await productsCollection.find(query).toArray()
+            res.send(productsArray)
+
+        })
+
+    }
+    finally {
+        // await client.close();
+
+    }
+
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
