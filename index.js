5325d9285f5eda6ae011a8a31101aba1dec260c4
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-25 03:46:54 +0600

    create api to get single user info by email also implement jwtVerify middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jwt = require('jsonwebtoken');
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
@@ -15,14 +16,46 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.8dwzm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    // If Token is undefined or not ok
+    if (!authHeader) {
+        return res.status(401).send({
+            message: 'Unauthorized access'
+        })
+    }
+    // Have a token but check if it is right or not
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+
+        // if error is occur means token is right but dont have permission to access 
+        if (err) {
+            return res.status(403).send({
+                message: 'Forbidden access'
+            })
+
+        }
+        req.decoded = decoded;
+        next();
+
+    });
+}
+
+
+
+
+
 const run = async () => {
     try {
         await client.connect();
         const productsCollection = client.db('manufacturerWebsite').collection('products');
+        const allUsersCollection = client.db('manufacturerWebsite').collection('users');
+        const allOrdersCollection = client.db('manufacturerWebsite').collection('orders');
 
 
 
         // ALL GET Method
+        // get all products
         app.get('/products', async (req, res) => {
             const query = {};
             const productsArray = await productsCollection.find(query).toArray()
@@ -30,6 +63,37 @@ const run = async () => {
 
         })
 
+
+        // get single user info by email
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const result = await allUsersCollection.findOne(query);
+
+            res.send(result)
+
+        })
+
+
+
+
+
+        // ALL PUT Method
+        // PUT - USER
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email };
+            const options = { upsert: true };
+            const userDoc = {
+                $set: user,
+            };
+            const result = await allUsersCollection.updateOne(filter, userDoc, options);
+            // JWT - Token
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+            res.send({ result, token })
+
+        })
     }
     finally {
         // await client.close();
