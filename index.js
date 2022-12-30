789b8412149b21c11df752712d57270cfe209d28
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-12-30 13:26:52 +0600

    nothing

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const verifyJWT = (req, res, next) => {
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
 
-        // if error is occur means token is right but dont have permission to access 
+        // if error is occur means token is  right but dont have permission to access 
         if (err) {
             return res.status(403).send({
                 message: 'Forbidden access'
